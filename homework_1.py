a59a71350c4cf4e83b407b879c9db309eda585fc
Philipp <philipp.steinforth@web.de>
2021-05-02 19:39:25 +0200

    Homework 1 - Keine Ahnung wo der Fehler liegt

--- homework_1.py
+++ homework_1.py
@@ -1,3 +1,119 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#analytisch für Aufgabenteil a
+
+a = 1
+h = 0.5
+x0 = 0.5
+
+x = np.linspace(0, 7, 256)
+y = x0*np.exp(-a*x)
+
+plt.plot(x,y)
+plt.xlabel('$t$')
+plt.ylabel('$x(t)$')
+
+#numerisch für Teil a
+
+i = h
+n = i+h
+x = np.linspace(0, h, 2)
+y = x0+(h*(-a*x))
+plt.plot (x, y, color='red')
+while(n<7.5):
+    x = np.linspace(i, n, 2)
+    y = y+(h*(-a*y))
+    plt.plot(x, y, color='red')
+    i = n
+    n = n+h
+    
+plt.show()
+
+#BIS HIER KLAPPTS!!!
+    
+#analytisch für Teil b
+
+h = 0.1
+
+x = np.linspace(0, 7, 256)
+y = x0*np.exp(-a*x)
+
+plt.plot(x,y)
+plt.xlabel('$t$')
+plt.ylabel('$x(t)$')
+
+#numerisch für Teil b
+
+i = h
+n = i+h
+x = np.linspace(0, h, 2)
+y = x0+(h*(-a*x))
+plt.plot (x, y, color='red')
+while(n<7.1):
+    x = np.linspace(i, n, 2)
+    y = y+(h*(-a*y))
+    plt.plot(x, y, color='red')
+    i = n
+    n = n+h
+    
+plt.show()
+    
+#analytisch für Teil c
+
+h = 0.01
+
+x = np.linspace(0, 7, 256)
+y = x0*np.exp(-a*x)
+
+plt.plot(x,y)
+plt.xlabel('$t$')
+plt.ylabel('$x(t)$')
+
+#numerisch für Teil c
+
+i = h
+n = i+h
+x = np.linspace(0, h, 2)
+y = x0+(h*(-a*x))
+plt.plot (x, y, color='red')
+while(n<7.01):
+    x = np.linspace(i, n, 2)
+    y = y+(h*(-a*y))
+    plt.plot(x, y, color='red')
+    i = n
+    n = n+h
+    
+plt.show()
+    
+#analytisch für teil d
+
+a = 3
+h = 0.5
+x0 = 3
+
+x = np.linspace(0, 7, 256)
+y = x0*np.exp(-a*x)
+
+plt.plot(x,y)
+plt.xlabel('$t$')
+plt.ylabel('$x(t)$')
+
+#numerisch für teil d
+
+i = h
+n = i+h
+x = np.linspace(0, h, 2)
+y = x0+(h*(-a*x))
+plt.plot (x, y, color='red')
+while(n<7.5):
+    x = np.linspace(i, n, 2)
+    y = y+(h*(-a*y))
+    plt.plot(x, y, color='red')
+    i = n
+    n = n+h
+    
+plt.show()
+
+
+
